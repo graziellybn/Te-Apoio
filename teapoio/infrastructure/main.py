06fdba1098b8712d1197d30a86a5c8e24eeb93ba
DevPKauan <pedrokauancardosoda@gmail.com>
2026-02-08 18:39:13 +0000

    Ajeitando_funções

--- teapoio/infrastructure/main.py
+++ teapoio/infrastructure/main.py
@@ -17,7 +17,7 @@ def main():
     print("Responsável:", resp.nome, "-", resp.tipo_responsavel)
     print("Crianças:")
     for c in resp.listar_criancas():
-        print(f"Nome: {c.nome}, Idade: {c.idade}, Responsável: {c.responsavel}, Nível de suporte: {c.nivel_suporte}")
+        print(f"Nome: {c.nome}\nIdade: {c.idade}\nNível de suporte: {c.nivel_suporte}\n")
 
 if __name__ == "__main__":
     main()
