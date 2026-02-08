fbdcc3e43c067bb490bdc60b18d4ff7e0cb4043e
DevPKauan <pedrokauancardosoda@gmail.com>
2026-02-08 14:37:22 +0000

    Co-authored-by: ramona-dev <ramona-dev@users.noreply.github.com>

---
+++ teapoio/domain/pessoa.py
@@ -0,0 +1,7 @@
+class Pessoa:
+    def __init__(self, nome, idade, cpf, email, telefone):
+        self.nome = nome
+        self.idade = idade
+        self.cpf = cpf
+        self.email = email
+        self.telefone = telefone
---
+++ teapoio/domain/responsavel.py
@@ -0,0 +1,4 @@
+class Responsavel(Pessoa):
+    def__init__(nome, idade, cpf, email, telefone)
+
+    super().__init__(nome, idade, cpf, email, telefone)
