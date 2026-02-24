af4b9caddb719cf934771100a9dc7620246f9b55
DevPKauan <pedrokauancardosoda@gmail.com>
2026-02-24 22:23:33 +0000

    Atualizando_Main

--- teapoio/infrastructure/main.py
+++ teapoio/infrastructure/main.py
@@ -1,6 +1,5 @@
-# teapoio/infrastructure/main.py
 from teapoio.infrastructure.cli import menu
 
 if __name__ == "__main__":
-    menu()python -m teapoio.infrastructure.main
-# Pra rodar a main diretamente: 
+    menu()
+# Pra rodar a main diretamente: python -m teapoio.infrastructure.main
